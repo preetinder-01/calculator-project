e66df0df30912dd07aef4520670ea955482ececf
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2020-05-04 21:23:30 +0530

    just updated the version

--- calculator.js
+++ calculator.js
@@ -1,16 +1,77 @@
-// function for display
-function dis(val){
-document.getElementById("input").value+=val;
+const numbers = document.querySelectorAll(".nmb")
+const operators = document.querySelectorAll(".optr")
+const equalSign = document.querySelector(".equal")
+const clearBtn = document.querySelector(".clear")
+const calculatorScreen = document.querySelector(".calculator_screen")
+let currentInput = '0'
+let calculationOperator = ' '
+let prevInput = '0'
+const updateScreen = (number) =>{
+      calculatorScreen.value = number
 }
-// function that clears the display
-function clr(){
-document.getElementById("input").value="";
+const inputOperator = (operator) =>{
+    prevInput = currentInput
+    calculationOperator = operator
+    currentInput = '0'
+}
+
+const clearAll = () =>{
+    prevInput = '0'
+    calculationOperator = ''
+    currentInput = '0'
+}
+operators.forEach((operator) =>{
+    operator.addEventListener("click", (event) =>{
+        //console.log(event.target.value)
+        inputOperator(event.target.value)
+    })
+})
+numbers.forEach((number) =>{
+    number.addEventListener("click", (event) =>{
+          inputNumber(event.target.value)
+          updateScreen(currentInput)
+    })
+})
+equalSign.addEventListener("click", () =>{
+    calculate()
+    updateScreen(currentInput)
+    //console.log("equal button is pressed")
+})
+clearBtn.addEventListener("click", () =>{
+    clearAll()
+    updateScreen(currentInput)
+})
+const inputNumber = (number) =>{
+    if (currentInput === '0') {
+        currentInput = number
+    }
+    else{
+      currentInput += number
+    }
 }
 
 
-//  function of evlauation
-function evalu(){
-    let a = document.getElementById("input").value;
-    let b = eval(a);
-    document.getElementById("input").value = b;
+
+const calculate = () =>{
+    let result = 0
+    prevInput = parseInt(prevInput)
+    currentInput = parseInt(currentInput)
+    switch(calculationOperator) {
+        case '+':
+            result = prevInput + currentInput
+            break;
+        case '*':
+            result = prevInput * currentInput
+            break;
+        case '-':
+            result = prevInput - currentInput
+            break;
+        case '/':
+            result = prevInput / currentInput
+            break;
+        default:
+            return
+    }
+    currentInput = result.toString()
+    calculationOperator = ''
 }
